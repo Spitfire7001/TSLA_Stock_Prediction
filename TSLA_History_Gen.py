750f9876ecd7663e28ef33f1df08b7556337b30b
Spitfire7001 <117041102+Spitfire7001@users.noreply.github.com>
2025-03-20 21:30:50 -0400

    update csv file

--- TSLA_History_Gen.py
+++ TSLA_History_Gen.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 ticker = "TSLA"
 start_date = "2010-06-29"
-end_date = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")  # Tomorrow's date
+end_date = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
 
 data = yf.download(ticker, start=start_date, end=end_date, interval="1d")
 data.to_csv("TSLA_Data.csv")
